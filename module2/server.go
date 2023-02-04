3420c1df0885136ca1fdb68432432c2730e09e29
1k2222 <736587124@qq.com>
2023-02-04 23:28:36 +0800

    feat: httpserver新增优雅退出

--- module2/server.go
+++ module2/server.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
+	"time"
 )
 
 func HandlerGreet(w http.ResponseWriter, r *http.Request) {
@@ -21,6 +25,12 @@ func HandlerGreet(w http.ResponseWriter, r *http.Request) {
 	log.Printf("access log, IP: %s, Path: %s, HTTP Code: %d", r.Host, r.URL.Path, http.StatusOK)
 }
 
+func HandlerDelayedGreet(w http.ResponseWriter, r *http.Request) {
+	log.Printf("waiting for 10 seconds...")
+	time.Sleep(time.Second * 10)
+	HandlerGreet(w, r)
+}
+
 func HandlerHealthz(w http.ResponseWriter, r *http.Request) {
 	if _, err := w.Write([]byte("OK\n")); err != nil {
 		log.Printf("write response failed, err: %s", err.Error())
@@ -30,10 +40,31 @@ func HandlerHealthz(w http.ResponseWriter, r *http.Request) {
 }
 
 func RunServer() {
-	http.HandleFunc("/greet", HandlerGreet)
-	http.HandleFunc("/healthz", HandlerHealthz)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		panic(err)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/greet", HandlerGreet)
+	mux.HandleFunc("/delayed_greet", HandlerDelayedGreet)
+	mux.HandleFunc("/healthz", HandlerHealthz)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	go server.ListenAndServe()
+	listenSignal(context.Background(), server)
+}
+
+func listenSignal(ctx context.Context, httpSrv *http.Server) {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	var s os.Signal
+	select {
+	case s = <-sigs:
+		fmt.Printf("notify sigs %d\n", s)
+		if err := httpSrv.Shutdown(ctx); err != nil {
+			log.Printf("shutdown failed, err: %s", err.Error())
+		}
+		fmt.Println("http shutdown")
 	}
 }
 
